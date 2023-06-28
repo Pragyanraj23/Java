45eb828a3ff03115b46291154198ea52538ba6cc
Pragyan Raj <pragyanraj23@gmail.com>
2023-06-27 20:24:11 -0600

    Basics

---
+++ multiclass.java
@@ -0,0 +1,16 @@
+class multiclass{
+public static void main(String args[]){
+System.out.println("Multiclass ");
+}
+}
+class multiclass1{
+public static void main(String args[]){
+System.out.print("Multiclass 1");
+}
+}
+class multiclass2{
+public static void main(String args[]){
+System.out.print("Multiclass 2");
+}
+}
+
